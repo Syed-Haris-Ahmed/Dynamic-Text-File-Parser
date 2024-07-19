9ae9b5d04213a07b8343718043343e1ff20afcb4
Syed-Haris-Ahmed <bcsbs2112333@szabist.pk>
2024-07-20 00:03:53 +0500

    Changes to icons

--- src/UI2/activity/ConfigView.jsx
+++ src/UI2/activity/ConfigView.jsx
@@ -2,11 +2,14 @@ import React from "react"
 import './ActivityView.css'
 import InputField from "./field-components/InputField"
 import SelectField from "./field-components/SelectField"
+import CreateIcon from '@mui/icons-material/Create'
+import EmailIcon from '@mui/icons-material/Email'  // Example of another icon
 
 function ConfigView() {
     return (
         <div className="view-container config-container">
-            <InputField text = "Config Field 1" />
+            <InputField text="Configuration Name" icon={<CreateIcon />} />
+            <InputField text="Email" icon={<EmailIcon />} />
             <SelectField/>
         </div>
     )
--- src/UI2/activity/field-components/InputField.jsx
+++ src/UI2/activity/field-components/InputField.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
-import AccountCircle from '@mui/icons-material/AccountCircle'
 import './Fields.css'
 
 function InputField(props) {
@@ -12,28 +11,28 @@ function InputField(props) {
                 display: 'flex',
                 alignItems: 'flex-end',
                 '&:focus-within .MuiSvgIcon-root': {
-                  color: '#00e9a2', // Change this to your desired focus color
+                  color: '#00e9a2',
                 },
                 '&:hover .MuiSvgIcon-root': {
-                  color: '#00e9a2', // Change this to your desired focus color
+                  color: '#00e9a2',
                 },
               }}
         >
-            <AccountCircle sx={{ color: 'white', mr: 1, my: 0.5 }} />
+            {React.cloneElement(props.icon, { sx: { color: 'white', mr: 1, my: 0.5 } })}
             <TextField
               id="input-with-sx"
               label={props.text}
               variant="standard"
               sx={{
-                '& .MuiInputLabel-root': { color: 'white' }, // Label color
-                '&:hover .MuiInputLabel-root': { color: '#92ffdf' }, // Label hover color
-                '& .MuiInputLabel-root.Mui-focused': { color: '#00e9a2' }, // Focused label color
-                '& .MuiInputLabel-root.Mui-focused:hover:': { color: '#00e9a2' }, // Focused label hover color
-                '& .MuiInputBase-root': { color: 'white' }, // Input text color
-                '& .MuiInputBase-input:hover': { color: '#92ffdf' }, // Input text hover color
-                '& .MuiInput-underline:hover:not(.Mui-disabled):before': { borderBottomColor: '#dcfff5' }, // Underline hover color
-                '& .MuiInput-underline:before': { borderBottomColor: 'white' }, // Underline color when not focused
-                '& .MuiInput-underline:after': { borderBottomColor: '#00e9a2' }, // Underline color when focused
+                '& .MuiInputLabel-root': { color: 'white' },
+                '&:hover .MuiInputLabel-root': { color: '#92ffdf' },
+                '& .MuiInputLabel-root.Mui-focused': { color: '#00e9a2' },
+                '& .MuiInputLabel-root.Mui-focused:hover:': { color: '#00e9a2' },
+                '& .MuiInputBase-root': { color: 'white' },
+                '& .MuiInputBase-input:hover': { color: '#92ffdf' },
+                '& .MuiInput-underline:hover:not(.Mui-disabled):before': { borderBottomColor: '#dcfff5' },
+                '& .MuiInput-underline:before': { borderBottomColor: 'white' },
+                '& .MuiInput-underline:after': { borderBottomColor: '#00e9a2' },
               }}
             />
         </Box>
